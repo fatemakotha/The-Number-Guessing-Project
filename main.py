460f1d885e5b0288f643770710863c85da127f8e
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-22 14:16:05 +0000

    Added three if statements

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ print("Welcome to The Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
 correct_answer = random.randint(1, 101) #picks a random number from 1 - 100
+print(type(correct_answer)) #prints: int
 print(f"The correct answer is {correct_answer}")
 
-guess = int(input("Enter a number from 1 - 100":))
+guess = int(input("Enter a number from 1 - 100: "))
+
+if guess == correct_answer:
+    print("You win")
+elif guess > correct_answer:
+    print("Too High")
+elif guess < correct_answer:
+    print("Too Low")
