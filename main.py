9bdbb683964c28b5cdbd2f53402f6d8d2a96e35c
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-22 13:55:57 +0000

    Added two print statements for the beginning of the game

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+print("Welcome to The Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+# correct_number = random.choice(1,101)
+# print(f"The correct answer is {correct_number}")
