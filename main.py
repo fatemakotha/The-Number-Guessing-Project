0b1c24792666fd10ccb571e43624a2192df088ff
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-22 14:01:35 +0000

    Variable set for correct answer

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import random
 print("Welcome to The Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
-# correct_number = random.choice(1,101)
-# print(f"The correct answer is {correct_number}")
+correct_answer = random.randint(1, 101) #picks a random number from 1 - 100
+# print(correct_answer)
