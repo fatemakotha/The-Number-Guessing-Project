59db3ede40b6ce7e20a882b2cc5f96ef692fc6cd
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-22 14:11:32 +0000

    Added a guess variable and converted it to integer

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ print("I'm thinking of a number between 1 and 100.")
 
 correct_answer = random.randint(1, 101) #picks a random number from 1 - 100
 print(f"The correct answer is {correct_answer}")
+
+guess = int(input("Enter a number from 1 - 100":))
