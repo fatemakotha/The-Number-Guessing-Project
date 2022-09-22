f58bcea40a5ed6081c304eef5407535ac7bdcdf5
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-22 14:02:28 +0000

    Added a print statement

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ print("Welcome to The Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
 correct_answer = random.randint(1, 101) #picks a random number from 1 - 100
-# print(correct_answer)
+print(f"The correct answer is {correct_answer}")
