8fa9ed575ca00119db40966db30e084d2c686c35
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-22 14:23:08 +0000

    Added a variable to ask whether player wants the easy/hard level

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ correct_answer = random.randint(1, 101) #picks a random number from 1 - 100
 print(type(correct_answer)) #prints: int
 print(f"The correct answer is {correct_answer}")
 
+question = input("Do you want the Easy or the Hard level?")
+
 guess = int(input("Enter a number from 1 - 100: "))
+
+
 
 if guess == correct_answer:
     print("You win")
 elif guess > correct_answer:
     print("Too High")
+    
 elif guess < correct_answer:
     print("Too Low")
